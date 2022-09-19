bcc338f8a16cdc77b4f20f9930bc23317848335c
Callylove <nwekechidimma88@gmail.com>
2022-09-19 15:59:58 -0700

    edited 2

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 import sys
-sys.stdderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
