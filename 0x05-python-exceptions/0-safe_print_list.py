3b00dfd24bf398b77bce093507f35ecb947d845c
Callylove <nwekechidimma88@gmail.com>
2022-10-10 12:57:03 -0700

    python exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
+    total = 0
+    for i in range(x):
         try:
-            print(my_list[m], end="")
-            n += 1
+            print(f"{my_list[i]}", end="")
+            total += 1
         except IndexError:
             break
     print()
-    return n
+    return(total)
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as error:
-        import sys
-        print("Exception: {}".format(error), file=sys.stderr)
-        return False
-    else:
-        return True
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
